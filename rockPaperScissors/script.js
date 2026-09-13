48e14f0f7a254e537fa78aa1711ef6d753bd73e0
b1ank-yc <ae_cont_acc@aol.com>
2026-09-13 23:14:28 +0200

    rps-ui added

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,11 +1,3 @@
-// 1. player 1 human choses 
-// 2. player 2 ai choses randomly
-// 3. choices are compared
-// 4. winner gets a point
-// 5. checks for point limit
-// 6. game continues or ends
-
-
 let getRandomInt = (max) => Math.floor(Math.random() * max);
 
 function computerChoice() {
@@ -28,47 +20,101 @@ function compareChoices(player, computer) {
     let p1 = player;
     let p2 = computer;
 
-    if (p1 === p2) return "Draw!";
+    if (p1 === p2) return 0;
 
     if (p1 === "Rock" && p2 === "Scissors" ||
         p1 === "Paper" && p2 === "Rock" ||
         p1 === "Scissors" && p2 === "Paper" ) {
-            return "You Win!";
-    } else { return "You Lose!"}
+            return 1;
+    } else { return 2}
 }
 
-function playRound() {
-    let p1 = prompt("Rock Paper or Scissors?");
-    let p2 = computerChoice();
-    
-    console.log(`You choose: ${p1}`);
-    console.log(`Opponent choose: ${p2}`);
+let p1Score = 0;
+let p2Score = 0;
 
-    result = compareChoices(p1, p2);
+let p1Overall = 0;
+let p2Overall = 0;
 
-    console.log("\n" + result + "\n\n\n");
+const buttons = document.querySelectorAll(".card");
 
-    return result;
-}
+const counter = document.querySelector(".rounds");
 
-function playGame() {
-    let roundNum = parseInt(prompt("To how many point do You want to play?"));
+const p1card = document.querySelector(".p1Card");
+const p2card = document.querySelector(".p2Card");
 
-    let p1 = 0;
-    let p2 = 0;
+const text = document.querySelector(".roundResult > label");
 
-    while (p1 < roundNum && p2 < roundNum) {
-        let result = playRound();
+const p1Text = document.querySelector(".p1Score");
+const p2Text = document.querySelector(".p2Score");
 
-        if (result === "You Win!") {
-            p1++;
-        } else if (result === "You Lose!") {
-            p2++;
+p1Text.textContent = `Player's Score is: ${p1Score} / 5`;
+p2Text.textContent = `Opponent's Score is: ${p2Score} / 5`;
+
+counter.textContent = `${p1Overall} - ${p2Overall}`;
+
+text.textContent = "What will you choose?";
+
+p1card.classList.add("cardStylePreset")
+p2card.classList.add("cardStylePreset")
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        const val = event.currentTarget.dataset.value;
+        const p2 = computerChoice();    
+        let result = compareChoices(val, p2);
+
+        p1card.textContent = val;
+        p2card.textContent = p2;
+
+        if (result === 0) {
+            text.textContent = "a Draawww~~!!!!";
+            text.style.color = "#333333";
+            p1card.style.border = "2px solid #333333";
+            p2card.style.border = "2px solid #333333";
+            p1card.style.backgroundColor = "#333333";
+            p2card.style.backgroundColor = "#333333";
+            p1card.style.color = "#C3C3C3";
+            p2card.style.color = "#C3C3C3";
+            
+        } else if (result === 1) {
+            text.textContent = "Player Won!";
+            p1card.style.border = "2px solid lightgreen";
+            p2card.style.border = "2px solid lightcoral";
+            p1card.style.color = "#C3C3C3";
+            p2card.style.color = "#C3C3C3";
+            p1Score += 1;
+            p1Text.textContent = `Player's Score is: ${p1Score} / 5`;
+        } else if (result === 2) {
+            text.textContent = "Opponent Won!";
+            p1card.style.border = "2px solid lightcoral";
+            p2card.style.border = "2px solid lightgreen";
+            p1card.style.color = "#C3C3C3";
+            p2card.style.color = "#C3C3C3";
+            p2Score += 1;
+            p2Text.textContent = `Opponent's Score is: ${p2Score} / 5`;
         }
-    }
 
-    if (p1 === roundNum) console.log(`You won the Game!\n\nYour Score: ${p1}\nOpponent's Score: ${p2}`);
-    if (p2 === roundNum) console.log(`You lost the Game!\n\nYour Score: ${p1}\nOpponent's Score: ${p2}`); 
-}
+        if (p1Score > 4) {
+             text.textContent = `Player..!\nYou Won That Match!`;
+             p1Overall += 1;
+             counter.textContent = `${p1Overall} - ${p2Overall}`;
+             p1Score = 0;
+             p2Score = 0;
+             p1Text.textContent = `Player's Score is: ${p1Score} / 5`;
+             p2Text.textContent = `Opponent's Score is: ${p2Score} / 5`;
+             p1card.style.backgroundColor = "#333333";
+            p2card.style.backgroundColor = "#333333";
+        } else if (p2Score > 4) {
+             text.textContent = `Opponent..!\nYou Won That Match!`;
+             p2Overall += 1;
+             counter.textContent = `${p1Overall} - ${p2Overall}`;
+             p1Score = 0;
+             p2Score = 0;
+             p1Text.textContent = `Player's Score is: ${p1Score} / 5`;
+             p2Text.textContent = `Opponent's Score is: ${p2Score} / 5`;
+             p1card.style.backgroundColor = "#333333";
+            p2card.style.backgroundColor = "#333333";
+        }
+    });
+});
 
-// playGame();
